3cd0af4e66249dc1462c46f555966289d10c78ec
goir <goir@users.noreply.github.com>
2014-06-19 11:54:35 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='uwsgicachetop',
+    version='1.0.0',
+    url='https://github.com/goir/uwsgicachetop',
+    download_url = 'https://github.com/goir/uwsgicachetop/archive/1.0.0.tar.gz',
+    license='GPLv2',
+    author='Goir',
+    author_email='goir@goirware.de',
+    scripts=['uwsgicachetop'],
+    description='Top like utility for uwsgi caches',
+    keywords = ['uwsgi', 'cache', 'top'],
+    classifiers = [],
+)
